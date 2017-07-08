257d2d89111dd77599ae2f88224685c59eddae6e
David Stainton <dstainton415@gmail.com>
2017-07-08 06:51:28 +0000

    Make Rekey test work without testify

--- noise_test.go
+++ noise_test.go
@@ -4,7 +4,6 @@ import (
 	"encoding/hex"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	. "gopkg.in/check.v1"
 )
 
@@ -466,9 +465,7 @@ func (NoiseSuite) TestHandshakeRollback(c *C) {
 	c.Assert(msg, DeepEquals, expected)
 }
 
-func TestRekey(t *testing.T) {
-	assert := assert.New(t)
-
+func (NoiseSuite) TestRekey(c *C) {
 	rng := new(RandomInc)
 
 	clientStaticKeypair := DH25519.GenerateKeypair(rng)
@@ -494,38 +491,38 @@ func TestRekey(t *testing.T) {
 	serverHs := NewHandshakeState(serverConfig)
 
 	clientHsMsg, _, _ := clientHs.WriteMessage(nil, nil)
-	assert.Equal(32, len(clientHsMsg), "client handshake message is unexpected size")
+	c.Assert(32, Equals, len(clientHsMsg))
 
 	serverHsResult, _, _, err := serverHs.ReadMessage(nil, clientHsMsg)
-	assert.NoError(err, "server failed to read client handshake message")
-	assert.Equal(0, len(serverHsResult), "server result message is unexpected size")
+	c.Assert(err, IsNil)
+	c.Assert(0, Equals, len(serverHsResult))
 
 	serverHsMsg, csR0, csR1 := serverHs.WriteMessage(nil, nil)
-	assert.Equal(48, len(serverHsMsg), "server handshake message is unexpected size")
+	c.Assert(48, Equals, len(serverHsMsg))
 
 	clientHsResult, csI0, csI1, err := clientHs.ReadMessage(nil, serverHsMsg)
-	assert.NoError(err, "client failed to read server handshake message")
-	assert.Equal(0, len(clientHsResult), "client result message is unexpected size")
+	c.Assert(err, IsNil)
+	c.Assert(0, Equals, len(clientHsResult))
 
 	clientMessage := []byte("hello")
 	msg := csI0.Encrypt(nil, nil, clientMessage)
 	res, err := csR0.Decrypt(nil, nil, msg)
-	assert.Equal(clientMessage, res, "server received unexpected message")
+	c.Assert(string(clientMessage), Equals, string(res))
 
 	oldK := csI0.k
 	csI0.Rekey()
-	assert.NotEqual(oldK, csI0.k, "should NOT be equal")
+	c.Assert(oldK == csI0.k, Equals, false)
 	csR0.Rekey()
 
 	clientMessage = []byte("hello again")
 	msg = csI0.Encrypt(nil, nil, clientMessage)
 	res, err = csR0.Decrypt(nil, nil, msg)
-	assert.Equal(clientMessage, res, "server received unexpected message")
+	c.Assert(string(clientMessage), Equals, string(res))
 
 	serverMessage := []byte("bye")
 	msg = csR1.Encrypt(nil, nil, serverMessage)
 	res, err = csI1.Decrypt(nil, nil, msg)
-	assert.Equal(serverMessage, res, "client received unexpected message")
+	c.Assert(string(serverMessage), Equals, string(res))
 
 	csR1.Rekey()
 	csI1.Rekey()
@@ -533,12 +530,12 @@ func TestRekey(t *testing.T) {
 	serverMessage = []byte("bye bye")
 	msg = csR1.Encrypt(nil, nil, serverMessage)
 	res, err = csI1.Decrypt(nil, nil, msg)
-	assert.Equal(serverMessage, res, "client received unexpected message")
+	c.Assert(string(serverMessage), Equals, string(res))
 
 	// only rekey one side, test for failure
 	csR1.Rekey()
 	serverMessage = []byte("bye again")
 	msg = csR1.Encrypt(nil, nil, serverMessage)
 	res, err = csI1.Decrypt(nil, nil, msg)
-	assert.NotEqual(serverMessage, res, "client received unexpected message")
+	c.Assert(string(serverMessage) == string(res), Equals, false)
 }
